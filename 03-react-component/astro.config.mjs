16368801dd7cb20e2638a7987ab90f133cd97981
Braulio <braulio.diez@gmail.com>
2025-10-25 10:39:53 +0200

    update 03 react

---
+++ 03-react-component/astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig, envField } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  env: {
+    schema: {
+      CONTENT_ISLAND_SECRET_TOKEN: envField.string({
+        context: 'server',
+        access: 'secret',
+        optional: false,
+        default: 'INFORM_VALID_TOKEN',
+      }),
+    },
+  },
+});
